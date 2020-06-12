e68d5b5cab9b84f865fc6132f24b1ee3285178bf
ChestnutsRiver <595810733@qq.com>
2020-06-12 13:46:07 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 按需导入不能分开写，否则Eslint会报错
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
